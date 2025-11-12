2a0e5a04af281806c3e992a3b448b7b0da12ff63
Amit <hazeydays@gmail.com>
2025-11-12 15:05:35 -0800

    added email service thru emailjs

--- index.js
+++ index.js
@@ -19,3 +19,34 @@ function toggleModal() {
    isModalOpen = true; 
    document.body.classList += " modal--open" 
 }
+
+function exitSuccess() {
+   const success = document.querySelector('.contact__overlay--success')
+   success.classList.remove("contact__overlay--visible") 
+}
+
+function contact(event) {
+    event.preventDefault();
+    const loading = document.querySelector('.contact__overlay--loading')
+    const success = document.querySelector('.contact__overlay--success')
+    loading.classList += " contact__overlay--visible"
+
+    emailjs
+     .sendForm(
+        'service_jfcdtnl',
+        'template_lv7yvan',
+        event.target,
+         'vcffZtLfc9UKX7XCR'
+     ).then(() => {
+        loading.classList.remove("contact__overlay--visible")
+        success.classList += " contact__overlay--visible"
+     }).catch(() => {
+        loading.classList.remove("contact__overlay--visible")
+        alert(
+            "The email service is temporarily unavailable. Please contact me directly on amirkhaynir@gmail.com"
+        );
+     })
+    
+}
+
+
